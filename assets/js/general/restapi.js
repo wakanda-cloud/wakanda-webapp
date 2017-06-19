a5b6d7d91cd245ff79cf66a84685c28a7e949720
Lucas Ventura <lnventura@hotmail.com>
2017-06-19 00:56:49 -0300

    fix delete url

--- assets/js/general/restapi.js
+++ assets/js/general/restapi.js
@@ -1,7 +1,7 @@
 var RestAPI = function(){}
 
 RestAPI.generateHerokuInstance = "https://wakanda-instance-generator.herokuapp.com/generate";
-RestAPI.fetchProjects = "https://wakanda-instance-generator.herokuapp.com/projects";
+RestAPI.projects = "https://wakanda-instance-generator.herokuapp.com/projects";
 RestAPI.registerUser = "https://wakanda-security.herokuapp.com/registerUser";
 RestAPI.login = "https://wakanda-security.herokuapp.com/login";
 RestAPI.fetchDashboardData = "https://wakandakingdom.herokuapp.com/fetchDashboardData";
